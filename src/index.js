6828fd5aff61666f688b5c9beea4018e1bca7603
nickc <nick@caveat.nyc>
2020-12-06 21:30:21 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./styles/index.scss";
+import "./images/yoda-stitch.jpg";
+import canvasExample from "./scripts/canvas";
+import Square from "./scripts/square";
+import { DOMExample } from "./scripts/DOMExample";
+//import things from scripts
+//import things from scss
